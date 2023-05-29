bc77a689ef58e0bda416bd12fecc70559a7ed24b
Dean Verjans <Developer.dean@outlook.com>
2023-05-29 23:09:36 +0200

    Resolve synced files

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,18 +1,5 @@
-<template>
-  {{ getSession }}
-</template>
+<template></template>
 
-<script setup lang="ts">
-import { useWallet } from '~/src/stores/wallet.store';
-import { storeToRefs } from 'pinia';
-
-const walletStore = useWallet();
-const { isConnected, getSession } = storeToRefs(walletStore);
-
-onMounted(async () => {
-  walletStore.setProvider('wallet-connect');
-  await walletStore.connect();
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
